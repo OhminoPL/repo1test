1aba70ade4b08cd1b0a54c7e0a22dc005c2278fb
Ohmino PL <me@example.com>
2025-03-10 10:18:04 +0100

    calc

--- kalkulator.py
+++ kalkulator.py
@@ -1,17 +1,21 @@
 def kalkulator():
     while True:
         # Pobranie wyrażenia od użytkownika
-        wyrazenie = input("Wpisz wyrażenie'koniec', aby zakończyć: ")
+        wyrazenie = input("Wpisz wyrażenie lub 'koniec', aby zakończyć: ")
 
         # Sprawdzenie, czy użytkownik chce zakończyć
         if wyrazenie.lower() == "koniec":
-            print("koniec programu")
+            print("Koniec programu")
             break
 
         try:
             # Obliczenie wyniku za pomocą funkcji eval
             wynik = eval(wyrazenie)
             print(f"Wynik: {wynik}")
+        except SyntaxError:
+            print("Nieprawidłowe wyrażenie: błąd składni")
+        except ZeroDivisionError:
+            print("Nieprawidłowe wyrażenie: dzielenie przez zero")
         except Exception as e:
             print(f"Nieprawidłowe wyrażenie: {e}")
 
